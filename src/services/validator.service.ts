524c053038a2703efdcad2befa6ba044b8ded4a0
Tushar <56352683+Mandloi2309@users.noreply.github.com>
2024-09-27 17:39:37 +0100

    code cleanup

--- src/services/validator.service.ts
+++ src/services/validator.service.ts
@@ -74,80 +74,7 @@ class ValidatorService {
 
     const validationReport: ValidationReport = this.#buildReport(processedFile.fileName, validationDictionary.name, validationDictionary.version, sheetsValidationResults);
     
-    if (validationReport.status === 'valid') {
-      // Build weights dictionary
-      const weightsDictionary: { [schemaName: string]: Array<{ fieldName: string; fieldWeight: number; weightForModelType: string }> } = {};
-      validationDictionary.schemas.forEach(schema => {
-        schema.fields.forEach(field => {
-          const { meta } = field;
-          if ( meta && 'field_weight' in meta && 'weight_for_model_type' in meta && typeof meta.field_weight === 'number' && meta?.field_weight > 0 && typeof meta?.weight_for_model_type === 'string') {
-            const schemaName = `${schema.name}.${field.name}`;
-            weightsDictionary[schemaName] = weightsDictionary[schemaName] || [];
-            weightsDictionary[schemaName].push({
-              fieldName: field.name,
-              fieldWeight: meta.field_weight,
-              weightForModelType: meta.weight_for_model_type,
-            });
-          }
-        });
-      });
-    
-      // Calculate maximum scores
-      const maxScores = Object.values(weightsDictionary).reduce((acc, [{ fieldWeight, weightForModelType }]) => {
-        if (weightForModelType === 'pdx' || weightForModelType === 'both') acc.pdx += fieldWeight;
-        if (weightForModelType === 'invitro' || weightForModelType === 'both') acc.invitro += fieldWeight;
-        return acc;
-      }, { pdx: 0, invitro: 0 });
-    
-      const fieldsWithWeights = Object.keys(weightsDictionary);
-    
-      // Prepare patient and model dictionaries
-      const patientDictionary: { [patient_id: string]: any } = {};
-      const modelDictionary: { [model_id: string]: any } = {};
-    
-      processedFile.data.forEach(({ rows }, entityName) => {
-        rows.forEach((row: RowData) => {
-          const prefixedRow = Object.fromEntries(Object.entries(row).map(([key, value]) => [`${entityName}.${key}`, value]));
-          const modelId = row.model_id;
-          const patientId = row.patient_id;
-    
-          if (modelId) modelDictionary[modelId] = { ...modelDictionary[modelId], ...prefixedRow };
-          if (patientId) patientDictionary[patientId] = { ...patientDictionary[patientId], ...prefixedRow };
-        });
-      });
-    
-      // Merge patient data into model data
-      Object.keys(modelDictionary).forEach(modelKey => {
-        const modelData = modelDictionary[modelKey];
-        if (modelData.patient_id && patientDictionary[modelData.patient_id]) {
-          Object.assign(modelDictionary[modelKey], patientDictionary[modelData.patient_id]);
-        }
-      });
-    
-      // Compute model scores
-      const modelScores: { [model_id: string]: number } = {};
-    
-      Object.keys(modelDictionary).forEach(modelId => {
-        const modelData = modelDictionary[modelId];
-        const isInvitro = Object.keys(modelData).some(key => key.includes('cell_model'));
-        const modelType = isInvitro ? 'invitro' : 'pdx';
-    
-        modelScores[modelId] = fieldsWithWeights.reduce((score, key) => {
-          const value = modelData[key];
-          const isValueValid = value && !['not provided', 'not collected'].includes(value.toLowerCase());
-          if (isValueValid) {
-            const weight = weightsDictionary[key][0].fieldWeight;
-            return score + weight;
-          }
-          return score;
-        }, 0);
-    
-        const maxScore = modelType === 'invitro' ? maxScores.invitro : maxScores.pdx;
-        modelScores[modelId] = (modelScores[modelId] / maxScore) * 100; // Calculate percentage
-      });
-    
-      console.log('Model Scores:', modelScores);
-    }
+    const modelScore = this.#calculateModelScore(validationReport.status, validationDictionary, processedFile.data)
     
     return await Promise.resolve(validationReport);
   }
@@ -204,7 +131,6 @@ class ValidatorService {
     return sheetValidationResult;
   }
   
-
   getSchemaNameFromFileName(fileName: string): string {
     const idx = fileName.indexOf('.');
     return fileName.slice(0, idx);
@@ -292,6 +218,88 @@ class ValidatorService {
       message: newMessage
     };
     return pdcmError;
+  }
+
+  #calculateModelScore(status: string, validationDictionary: SchemasDictionary, data: Map<string, SheetData>){
+    const modelScores: { [model_id: string]: number } = {};
+    if (status === 'invalid') {
+      return modelScores;
+
+    }
+    
+    // Build weights dictionary
+    const weightsDictionary: { [schemaName: string]: Array<{ fieldName: string; fieldWeight: number; weightForModelType: string }> } = {};
+    validationDictionary.schemas.forEach(schema => {
+      schema.fields.forEach(field => {
+        const { meta } = field;
+        if ( meta && 'field_weight' in meta && 'weight_for_model_type' in meta && typeof meta.field_weight === 'number' && meta?.field_weight > 0 && typeof meta?.weight_for_model_type === 'string') {
+          const schemaName = `${schema.name}.${field.name}`;
+          weightsDictionary[schemaName] = weightsDictionary[schemaName] || [];
+          weightsDictionary[schemaName].push({
+            fieldName: field.name,
+            fieldWeight: meta.field_weight,
+            weightForModelType: meta.weight_for_model_type,
+          });
+        }
+      });
+    });
+  
+    // Calculate maximum scores
+    const maxScores = Object.values(weightsDictionary).reduce((acc, [{ fieldWeight, weightForModelType }]) => {
+      if (weightForModelType === 'pdx' || weightForModelType === 'both') acc.pdx += fieldWeight;
+      if (weightForModelType === 'invitro' || weightForModelType === 'both') acc.invitro += fieldWeight;
+      return acc;
+    }, { pdx: 0, invitro: 0 });
+  
+    const fieldsWithWeights = Object.keys(weightsDictionary);
+  
+    // Prepare patient and model dictionaries
+    const patientDictionary: { [patient_id: string]: any } = {};
+    const modelDictionary: { [model_id: string]: any } = {};
+  
+    data.forEach(({ rows }, entityName) => {
+      rows.forEach((row: RowData) => {
+        const prefixedRow = Object.fromEntries(Object.entries(row).map(([key, value]) => [`${entityName}.${key}`, value]));
+        const modelId = row.model_id;
+        const patientId = row.patient_id;
+  
+        if (modelId) modelDictionary[modelId] = { ...modelDictionary[modelId], ...prefixedRow };
+        if (patientId) patientDictionary[patientId] = { ...patientDictionary[patientId], ...prefixedRow };
+      });
+    });
+  
+    // Merge patient data into model data
+    Object.keys(modelDictionary).forEach(modelKey => {
+      const modelData = modelDictionary[modelKey];
+      if (modelData.patient_id && patientDictionary[modelData.patient_id]) {
+        Object.assign(modelDictionary[modelKey], patientDictionary[modelData.patient_id]);
+      }
+    });
+  
+    // Compute model scores
+    
+  
+    Object.keys(modelDictionary).forEach(modelId => {
+      const modelData = modelDictionary[modelId];
+      const isInvitro = Object.keys(modelData).some(key => key.includes('cell_model'));
+      const modelType = isInvitro ? 'invitro' : 'pdx';
+  
+      modelScores[modelId] = fieldsWithWeights.reduce((score, key) => {
+        const value = modelData[key];
+        const isValueValid = value && !['not provided', 'not collected'].includes(value.toLowerCase());
+        if (isValueValid) {
+          const weight = weightsDictionary[key][0].fieldWeight;
+          return score + weight;
+        }
+        return score;
+      }, 0);
+  
+      const maxScore = modelType === 'invitro' ? maxScores.invitro : maxScores.pdx;
+      modelScores[modelId] = (modelScores[modelId] / maxScore) * 100; // Calculate percentage
+    });
+  
+    console.log('Model Scores:', modelScores);
+    return modelScores;
   }
 }
 
